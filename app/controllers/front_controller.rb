da8155c841b7d37e163e09c5b85c865928d749fe
Anton Kostin <schnibba@gmail.com>
2018-01-21 22:05:27 +0300

    Готов полный перевод всех статичных страниц.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -3,8 +3,17 @@ class FrontController < ApplicationController
 
   before_action :find_setting
   before_action :is_18?
+  before_action :set_locale
 
   private
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
 
   def find_setting
     @setting = Setting.first
--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,22 @@ Rails.application.routes.draw do
     resources :settings
   end
 
-  scope module: :front do
-    root 'static_pages#home'
-    get '/about', to: 'static_pages#about'
-    get '/contacts', to: 'static_pages#contacts'
-    get '/technology', to: 'static_pages#technology'
-    get '/visit', to: 'static_pages#visit'
-    get '/club', to: 'static_pages#club'
-    get '/terra', to: 'static_pages#terra'
-    get '/age_confirmation', to: 'static_pages#age_confirmation'
-    resources :vintages, path: :wines do
-      resources :wines, path: :vintages
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    scope module: :front do
+      root 'static_pages#home'
+      get '/about', to: 'static_pages#about'
+      get '/contacts', to: 'static_pages#contacts'
+      get '/technology', to: 'static_pages#technology'
+      get '/visit', to: 'static_pages#visit'
+      get '/club', to: 'static_pages#club'
+      get '/terra', to: 'static_pages#terra'
+      get '/age_confirmation', to: 'static_pages#age_confirmation'
+      resources :vintages, path: :wines do
+        resources :wines, path: :vintages
+      end
     end
   end
+
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), via: :get
+  match '', to: redirect("/#{I18n.default_locale}/"), via: :get
 end
